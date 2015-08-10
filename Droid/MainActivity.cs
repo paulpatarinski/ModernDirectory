2384eba877bf1121deb0cc09dbde340aa9f41de6
Paul Patarinski <paulpatarinski@gmail.com>
2015-08-10 00:46:14 -0500

    Fixing the app name

--- Droid/MainActivity.cs
+++ Droid/MainActivity.cs
@@ -6,7 +6,7 @@ using RoundedBoxView.Forms.Plugin.Droid;
 
 namespace ModernDirectory.Droid
 {
-	[Activity (Label = "ModernDirectory.Droid", Icon = "@drawable/icon", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+	[Activity (Icon = "@drawable/icon", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
 	public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsApplicationActivity
 	{
 		protected override void OnCreate (Bundle bundle)
