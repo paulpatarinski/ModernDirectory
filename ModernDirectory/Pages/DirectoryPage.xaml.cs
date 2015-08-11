b988be07e4527138c29a5e8cae610cdfc0098ac2
Paul Patarinski <paulpatarinski@gmail.com>
2015-08-11 02:09:41 -0500

    Adding a sample search

--- ModernDirectory/Pages/DirectoryPage.xaml.cs
+++ ModernDirectory/Pages/DirectoryPage.xaml.cs
@@ -3,6 +3,11 @@ using System.ServiceModel.Channels;
 using ModernDirectory.ViewModels;
 using ModernDirectory.Models;
 using ModernDirectory.Services;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using System;
 
 namespace ModernDirectory.Pages
 {
@@ -25,6 +30,44 @@ namespace ModernDirectory.Pages
 			await Navigation.PushAsync (new DirectoryDetailPage(e.SelectedItem as Person));
 
 			peopleListview.SelectedItem = null;
+		}
+
+		ObservableCollection<Person> _originalSource;
+
+		/// <summary>
+		/// This is a temp implementation, make a server call when fully implemented
+		/// Source From https://blog.verslu.is/xamarin/finding-nemo-implementing-xamarin-forms-searchbar/
+		/// </summary>
+		/// <param name="sender">Sender.</param>
+		/// <param name="e">E.</param>
+		private async void SearchBar_OnTextChanged(object sender, TextChangedEventArgs e)
+		{
+			//todo : implement using server call
+			var people = peopleListview.ItemsSource as ObservableCollection<Person>;
+
+			if(people != null)
+			{
+				if (_originalSource == null)
+					_originalSource = people;
+				
+				peopleListview.IsRefreshing = true;
+
+				//Simulate long server call
+				await Task.Delay (TimeSpan.FromSeconds (2)).ContinueWith ((r) =>{
+
+					Device.BeginInvokeOnMainThread (() =>
+						{
+							if (string.IsNullOrWhiteSpace(e.NewTextValue))
+								peopleListview.ItemsSource = _originalSource;
+							else
+								peopleListview.ItemsSource = new ObservableCollection<Person>(_originalSource.Where(p => p.FullName.ToLower ().Contains(e.NewTextValue.ToLower ()) 
+									|| p.PhoneNumber.Contains (e.NewTextValue)));
+
+							peopleListview.IsRefreshing = false;
+						});
+				});
+			
+			}
 		}
 	}
 }
