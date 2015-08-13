49fe656041f41753a66a0a7f40b5f7e8fea2b3bf
Paul Patarinski <paulpatarinski@gmail.com>
2015-08-13 02:36:20 -0500

    Moving the auth header to a global attribute

--- ModernDirectory/Services/IGooglePlusApi.cs
+++ ModernDirectory/Services/IGooglePlusApi.cs
@@ -5,14 +5,13 @@ using System.Globalization;
 
 namespace ModernDirectory.Services
 {
+	[Headers("Authorization: Bearer")]
 	public interface IGooglePlusApi
 	{
 		[Get("/people/me/people/visible?maxResults={pageSize}&pageToken={nextPageToken}&fields=items(cover%2CdisplayName%2Cemails%2Cgender%2Cimage%2Cname%2Corganizations%2Fname)%2CnextPageToken")]
-		[Headers("Authorization: Bearer")]
 		Task<GooglePeoplePayload> GetPeopleAsync(int pageSize, string nextPageToken);
 
 		[Get("/people?query={searchQuery}&maxResults={pageSize}&pageToken={nextPageToken}&fields=items(displayName%2Cgender%2Cimage%2Cname)%2CnextPageToken")]
-		[Headers("Authorization: Bearer")]
 		Task<GooglePeoplePayload> SearchPeopleAsync(string searchQuery, int pageSize, string nextPageToken);
 	}
 }
